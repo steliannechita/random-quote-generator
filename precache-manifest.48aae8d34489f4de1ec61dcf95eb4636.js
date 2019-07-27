fba7f9c801b0dd2b2df51cb2c4e9382f5225d422
stelian nechita <steliannechita2013@gmail.com>
2019-07-27 17:07:01 +0100

    Updates

---
+++ precache-manifest.48aae8d34489f4de1ec61dcf95eb4636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcef990cf508c380d9c7e7468e7378e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "687d3dde83a2f8883784",
+    "url": "/static/css/main.3ceb95cb.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "687d3dde83a2f8883784",
+    "url": "/static/js/main.158f15c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
